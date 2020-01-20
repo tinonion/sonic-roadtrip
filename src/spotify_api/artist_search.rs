ee937cc575af9a9d5ada2dde505e03f9cb1dcc58
W. Hunter Jones <william.hunter.jones@gmail.com>
2020-01-20 18:21:04 -0500

    implemented singleton for spotify client and fixed some bugs

--- src/spotify_api/artist_search.rs
+++ src/spotify_api/artist_search.rs
@@ -8,16 +8,20 @@ static mut CLIENT: SpotifyClient = SpotifyClient::new();
 pub fn search_artist(query: &str) -> Vec<MinimalArtist> {
     let spotify = unsafe { CLIENT.take_spotify() };
 
-    let result = spotify.search_artist(query, 100, 0, Some(Country::UnitedStates));
-    let result = result.unwrap();
+    let result = spotify.search_artist(query, 10, 0, Some(Country::UnitedStates));
 
-    let first_page_artists = result.artists.items;
-
-    assert!(first_page_artists.len() >= 1);
+    let first_page_artists = match result {
+        Ok(search) => search.artists.items,
+        Err(e) => panic!("error found {:?} for query {}", e, query)
+    };
 
     first_page_artists.iter()
         .map(|full_artist| MinimalArtist::create(
             &full_artist.name, 
-            &full_artist.images[0].url))
+            if full_artist.images.len() >= 1 {
+                &full_artist.images[0].url
+            } else {
+                "IMAGE N/A"
+            }))
         .collect()
 }
